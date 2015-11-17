513b6dc8a575c171d3b797f39e1dd5623e680467
umamahem <umamohan@hp.com>
2015-11-17 12:38:50 +0530

    Update HelloServlet.java

--- src/main/java/org/hp/samples/HelloServlet.java
+++ src/main/java/org/hp/samples/HelloServlet.java
@@ -37,7 +37,7 @@ public class HelloServlet extends HttpServlet {
 		response.setContentType("text/plain");
 		response.setStatus(200);
 		PrintWriter writer = response.getWriter();
-		writer.println("Hello World Kunal and Uma !!!");
+		writer.println("Hello World Kunal and Uma.");
 		writer.close();
 	}
 }
